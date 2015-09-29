7fa90848a11f56bf9deb60aa4da6f4a70ea09ed7
Moritz Zimmer <moritz.zmmr@gmail.com>
2015-09-29 13:26:17 +0200

    Made jacoco tool version configurable

--- src/main/groovy/de/weltn24/gradle/plugins/JavaConventionsPluginExtension.groovy
+++ src/main/groovy/de/weltn24/gradle/plugins/JavaConventionsPluginExtension.groovy
@@ -4,4 +4,5 @@ class JavaConventionsPluginExtension {
     boolean runTestsParallel = false;
     int testThreads = (int) (Runtime.runtime.availableProcessors()/2);
     String assertjVersion = "3.2.0";
+    String jacocoVersion = "0.7.5.201505241946";
 }
